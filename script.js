103f09fb6448d0a6c517adb5c6620a26704fa71a
Mentalcase12 <109283519+Mentalcase12@users.noreply.github.com>
2024-02-19 18:25:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Function to toggle dark mode
-function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-}
 
-// Function to toggle light mode
-function toggleLightMode() {
-    const body = document.body;
-    body.classList.remove('dark-mode');
-}
-
-// Add event listeners for dark mode and light mode buttons
-document.getElementById('darkModeButton').addEventListener('click', toggleDarkMode);
-document.getElementById('lightModeButton').addEventListener('click', toggleLightMode);
